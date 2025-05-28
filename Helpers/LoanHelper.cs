820e9455c575a3e61acee43f3648a5a10875f997
justarunsthing <justarunsthing@outlook.com>
2025-05-28 18:07:42 +0100

    Added helper method

--- Helpers/LoanHelper.cs
+++ Helpers/LoanHelper.cs
@@ -1,7 +1,53 @@
-﻿namespace MVCSiteTemplate.Helpers
+﻿using MVCSiteTemplate.Models;
+
+namespace MVCSiteTemplate.Helpers
 {
     public class LoanHelper
     {
+        public Loan GetPayments(Loan loan)
+        {
+            loan.Payment = CalculatePayment(loan.Amount, loan.Rate, loan.Term);
+
+            var balance = loan.Amount; // Balance equals loan amount on first month
+            var totalInterest = 0.0m; //Interest paid on the month, 0 on first month
+            var monthlyInterest = 0.0m; // Same as above
+
+            // Subtracting interest from payment tells us how much went to the principal to pay off the loan
+            var monthlyPrincipal = 0.0m;
+            var monthlyRate = CalculateMonthlyRate(loan.Rate);
+
+            // Loop from 1 to the end of the term
+            for (int month = 1; month <= loan.Term; month++)
+            {
+                monthlyInterest = CalculateMonthlyInterest(balance, monthlyRate);
+
+                // Running total of interest, each month add to it
+                totalInterest += monthlyInterest;
+                monthlyPrincipal = loan.Payment - monthlyInterest;
+                balance -= monthlyPrincipal;
+
+                // Add to loan payment schedule
+                var loanPayment = new LoanPayment
+                {
+                    Month = month,
+                    Payment = loan.Payment,
+                    MonthlyPrincipal = monthlyPrincipal,
+                    MonthlyInterest = monthlyInterest,
+                    TotalInterest = totalInterest,
+                    Balance = balance
+                };
+
+                // Push loanPayment object into the loan model
+                loan.Payments.Add(loanPayment);
+            }
+
+            // Push values to the properties of the loan model
+            loan.TotalInterest = totalInterest;
+            loan.TotalCost = loan.Amount + totalInterest; // Amount borrowed plus total interest over life of the loan
+
+            return loan;
+        }
+
         private decimal CalculatePayment(decimal amount, decimal rate, int term)
         {
             var monthlyRate = CalculateMonthlyRate(rate);
