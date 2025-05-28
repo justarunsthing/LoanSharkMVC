e7dde87aae86628a526215223916639016d92ddd
justarunsthing <justarunsthing@outlook.com>
2025-05-28 17:46:18 +0100

    Added Loan class

---
+++ Models/Loan.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCSiteTemplate.Models
+{
+    public class Loan
+    {
+        public decimal Amount { get; set; }
+        public decimal Rate { get; set; }
+        public int Term { get; set; } // In months
+        public decimal Payment { get; set; } // Monthly payment
+        public decimal TotalInterest { get; set; } // Interest over life of the loan
+        public decimal TotalCost { get; set; } // Amount borrowed plus total interest
+    }
+}
