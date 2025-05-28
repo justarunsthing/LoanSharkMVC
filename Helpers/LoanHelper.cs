78da3ed8b5f1a5f9ffb885bb2d00dec4b3c6e3a5
justarunsthing <justarunsthing@outlook.com>
2025-05-28 17:58:57 +0100

    Added helper class and private methods

---
+++ Helpers/LoanHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace MVCSiteTemplate.Helpers
+{
+    public class LoanHelper
+    {
+        private decimal CalculatePayment(decimal amount, decimal rate, int term)
+        {
+            var monthlyRate = CalculateMonthlyRate(rate);
+            var amountDouble = Convert.ToDouble(amount);
+            var rateDouble = Convert.ToDouble(monthlyRate);
+            var paymentDouble = (amountDouble * rateDouble) / (1 - Math.Pow(1 + rateDouble, -term));
+
+            return Convert.ToDecimal(paymentDouble);
+        }
+
+        private decimal CalculateMonthlyRate(decimal rate)
+        {
+            return rate / 1200;
+        }
+
+        private decimal CalculateMonthlyInterest(decimal balance, decimal monthlyRate)
+        {
+            return balance * monthlyRate;
+        }
+    }
+}
